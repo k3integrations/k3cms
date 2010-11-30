d4422f2edb753ac0dcb16e0eada7bc84e98aa855
Tyler Rick <tyler@k3integrations.com>
2010-11-30 15:21:43 -0800

    K3::Pages::PageNotFound middleware now looks for a Page record based on the requested path/URL. If it finds a record, it renders it, otherwise it allows the request to continue as normal. If Rails returns with a 404, it displays "The page you have requested could not be found. Create it now?"

--- lib/k3/pages/page_not_found.rb
+++ lib/k3/pages/page_not_found.rb
@@ -1,23 +1,33 @@
 module K3
   module Pages
     class PageNotFound
+      unloadable
+
       def initialize(app, message = "Response Time")
         @app = app
         @message = message
       end
 
       def call(env)
-        @start = Time.now
-        @status, @headers, @response = @app.call(env)
-        @stop = Time.now
-        [@status, @headers, self]
-      end
+        pages = Page.where(:url => env['PATH_INFO']).all
+        Rails.logger.debug "... env['PATH_INFO']=#{env['PATH_INFO'].inspect}"
 
-      def each(&block)
-        if @headers["Content-Type"] and @headers["Content-Type"].include? "text/html"
-          block.call("<!-- #{Page.count} pages -->\n")
+        if pages.any? and (page = pages.first)
+          env['PATH_INFO'] = "/pages/#{page.id}"
+          Rails.logger.debug "... Serving page via #{env['PATH_INFO']}"
+          @status, @headers, @response = @app.call(env)
+
+        else
+          # Continue processing stack
+          @status, @headers, @response = @app.call(env)
+          if @status == 404
+            Rails.logger.debug "... PageNotFound: Handling 404"
+            [404, {"Content-Type" => "text/html"}, 'The page you have requested could not be found. Create it now? (if logged in)']
+
+          else
+            [@status, @headers, @response]
+          end
         end
-        @response.each(&block)
       end
     end
   end
