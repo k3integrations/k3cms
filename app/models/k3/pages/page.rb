e083e46c04201650c1ee78a1ce2c02055359d465
Tyler Rick <tyler@k3integrations.com>
2010-12-01 09:40:34 -0800

    Started set_default_title_or_url

--- app/models/k3/pages/page.rb
+++ app/models/k3/pages/page.rb
@@ -1,3 +1,11 @@
 class K3::Pages::Page < ActiveRecord::Base
   validates :title, :presence => true
+
+  def after_initialize
+    set_default_title_or_url
+  end
+
+  def set_default_title_or_url
+    self.title ||= 'test'
+  end
 end
