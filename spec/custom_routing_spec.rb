c3d1cbcba192ea619697dc11e0aac2396948aa1e
Tyler Rick <tyler@k3integrations.com>
2010-12-02 15:30:40 -0800

    Don't know how to test our custom routing. A controller spec doesn't work because we need to do get '/my_custom_url', not get :action.

---
+++ spec/custom_routing_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'app/controllers/k3/pages/pages_controller'
+
+describe K3::Pages::PagesController do
+
+  describe "when you request a page by its custom url" do
+   #it "it finds it and renders it" do
+   #  get '/my_custom_url'
+   #end
+  end
+
+end
