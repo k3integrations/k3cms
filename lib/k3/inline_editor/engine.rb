ec7ea6841373f83be1bb5de9b0006959eb9fe59b
Tyler Rick <tyler@k3integrations.com>
2010-12-10 14:28:38 -0800

    Add to config.action_view.javascript_expansions[:k3]:          'jquery.rest_in_place.js' must be loaded before          'inline_editor.js'        must be loaded before          'k3/inline_editor.js'

--- lib/k3/inline_editor/engine.rb
+++ lib/k3/inline_editor/engine.rb
@@ -8,6 +8,15 @@ module K3
   module InlineEditor
 
     class Engine < Rails::Engine
+      puts "#{self}"
+
+      #puts "config.action_view.javascript_expansions=#{config.action_view.javascript_expansions.inspect}"
+      config.action_view.javascript_expansions[:k3].concat [
+        'jquery.rest_in_place.js',
+        'inline_editor.js',
+        'k3/inline_editor.js',
+      ]
+
       initializer 'k3.inline_editor.add_cells_paths' do |app|
         Cell::Base.view_paths += [Pathname[__DIR__] + '../../../app/cells']
       end
