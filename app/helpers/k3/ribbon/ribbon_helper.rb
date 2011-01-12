3b187d1923bdd0bbac2d1c9e87a81f7ca4229ce2
Tyler Rick <tyler@k3integrations.com>
2011-01-12 14:45:38 -0800

    Added a really simple add_to_ribbon helper. Render all items that are added with this method.

--- app/helpers/k3/ribbon/ribbon_helper.rb
+++ app/helpers/k3/ribbon/ribbon_helper.rb
@@ -3,7 +3,14 @@ module K3::Ribbon::RibbonHelper
     puts "#{self} was included"
   end
 
+  def add_to_ribbon
+    @k3_ribbon_items ||= []
+    @k3_ribbon_items << capture { yield }
+    nil
+  end
+
   def edit_mode?
+    session[:edit_mode] = false if session[:edit_mode].nil?
     session[:edit_mode]
   end
 end
