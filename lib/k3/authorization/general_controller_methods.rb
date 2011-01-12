6ec2610aedeac91bad47925ed53e2fb5aff7aebe
Tyler Rick <tyler@k3integrations.com>
2011-01-12 11:52:45 -0800

    Made k3_successful_signin return url instead of redirecting to it, since that caused a DoubleRenderError

--- lib/k3/authorization/general_controller_methods.rb
+++ lib/k3/authorization/general_controller_methods.rb
@@ -1,27 +1,27 @@
 module K3
   module Authorization
     module GeneralControllerMethods
-      
+
       def self.included(base)
         base.helper_method :k3_logged_in?
       end
-      
+
       def k3_logged_in?
         k3_user.k3_guest?
       end
-      
+
       def k3_failed_authorization
         render :text => 'Failed Authorization'
       end
-      
+
       def k3_successful_signin
-        redirect_to(respond_to?(root_url, true) ? root_url : '/')
+        respond_to?(:root_url, true) ? root_url : '/'
       end
-      
+
       def k3_successful_signout
-        redirect_to(respond_to?(root_url, true) ? root_url : '/')
+        respond_to?(:root_url, true) ? root_url : '/'
       end
-      
+
     end
   end
 end
