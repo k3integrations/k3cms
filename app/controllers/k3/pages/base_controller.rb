8b347b20d5af158846d66b7191bc977aac1ce0f9
Tyler Rick <tyler@k3integrations.com>
2011-01-12 16:45:31 -0800

    This require is necessary so that we won't get this error when running specs: uninitialized constant CanCan (NameError)
    When running a Rails app, we don't have to worry about this because it
    is automatically required by the line Bundler.require(:default,
    Rails.env) but when we're not running in the context of an app, we
    aren't so lucky.

--- app/controllers/k3/pages/base_controller.rb
+++ app/controllers/k3/pages/base_controller.rb
@@ -1,13 +1,15 @@
+require 'k3_cancan'
+
 module K3
   module Pages
     class BaseController < ApplicationController
       # Needed for CanCan authorization
       include CanCan::ControllerAdditions
-      
+
       def current_ability
         @current_ability ||= K3::Pages::Ability.new(k3_user)
       end
-      
+
       rescue_from CanCan::AccessDenied do |exception|
         k3_authorization_required
       end
