88a41465c746b64237d14609c426dd6e1ccca217
Tyler Rick <tyler@k3integrations.com>
2011-01-13 09:37:46 -0800

    Using the new 'included' macro from ActiveSupport::Concern

--- lib/k3/authorization/drivers/devise.rb
+++ lib/k3/authorization/drivers/devise.rb
@@ -2,13 +2,15 @@ module K3
   module Authorization
     module Drivers
       module Devise
+        extend ActiveSupport::Concern
+
         private
-        
+
         # Devise specific
         def k3_user
           current_user || @k3_guest_user ||= K3::Authorization::GuestUser.new
         end
-        
+
         def k3_authorization_required
           if !user_signed_in?
             authenticate_user!
@@ -16,21 +18,21 @@ module K3
             k3_failed_authorization
           end
         end
-        
+
         def after_sign_in_path_for(resource)
           k3_successful_signin
         end
-        
+
         def after_sign_out_path_for(resource)
           k3_successful_signout
         end
-        
+
         public
-        
-        def self.included(base)
-          base.helper_method :k3_user, :k3_authorization_required
+
+        included do
+          helper_method :k3_user, :k3_authorization_required
         end
-          
+
       end
     end
   end
