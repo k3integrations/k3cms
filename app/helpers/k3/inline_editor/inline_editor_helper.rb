b70305d513d977e3c56448834ba309e54b4a3e70
Tyler Rick <tyler@k3integrations.com>
2011-01-17 14:54:12 -0800

    Added inline_editable helper, which lets us replace: -        %span.editable{"data-attribute" => "title", "data-formtype" => "inline_editor", "data-object" => "k3_blog_post", "data-url" => k3_blog_blog_post_path(@blog_post)} with much more concise: +        = inline_editable('span', @blog_post, :title) do

--- app/helpers/k3/inline_editor/inline_editor_helper.rb
+++ app/helpers/k3/inline_editor/inline_editor_helper.rb
@@ -2,7 +2,13 @@ module K3
   module InlineEditor
     module InlineEditorHelper
       def self.included(base)
-        puts "#{self} was included"
+        #puts "#{self} was included"
+      end
+
+      def inline_editable(tag_name, object, attr_name)
+        content_tag(tag_name, :class => 'editable', 'data-url' => url_for(object), 'data-object' => dom_class(object), 'data-attribute' => attr_name,  'data-formtype' => "inline_editor") do
+          capture { yield }
+        end
       end
     end
   end
