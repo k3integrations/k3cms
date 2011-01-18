62bb90effd724f1676b2c6c72574daa33b65adb8
Tyler Rick <tyler@k3integrations.com>
2011-01-17 16:55:43 -0800

    Moved some stuff out of k3_ribbon which shouldn't be there and into app/views/k3/pages/init.html.haml

--- app/cells/k3/pages_cell.rb
+++ app/cells/k3/pages_cell.rb
@@ -1,6 +1,10 @@
 class K3::PagesCell < Cell::Rails
   helper K3::InlineEditor::InlineEditorHelper
 
+  def init
+    render
+  end
+
   def last_saved_status
     @page = @opts[:page]
     if @page && !@page.new_record?
