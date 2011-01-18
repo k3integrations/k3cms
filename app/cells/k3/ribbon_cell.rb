896d6fd99670acf22463ce4b38b5a4b32f17dd2f
Tyler Rick <tyler@k3integrations.com>
2011-01-17 16:51:07 -0800

    Added the ability to add drawers to the ribbon (k3_ribbon_add_drawer helper, etc.)
    Moved some pages-specific stuff out to pages.

--- app/cells/k3/ribbon_cell.rb
+++ app/cells/k3/ribbon_cell.rb
@@ -7,8 +7,10 @@ class K3::RibbonCell < Cell::Base
 
   def show
     @page = @opts[:current_page]
-    @k3_ribbon_items = @opts[:k3_ribbon_items]
     render if k3_user.k3_permitted?(:view_ribbon)
   end
 
+  def drawer
+    render
+  end
 end
--- app/helpers/k3/ribbon/ribbon_helper.rb
+++ app/helpers/k3/ribbon/ribbon_helper.rb
@@ -1,11 +1,23 @@
 module K3::Ribbon::RibbonHelper
   def self.included(base)
-    puts "#{self} was included"
+    #puts "#{self} was included"
   end
 
-  def add_to_ribbon
+  def k3_ribbon
+    render_cell 'k3/ribbon', :show,
+      :k3_ribbon_items   => @k3_ribbon_items,
+      :k3_ribbon_drawers => @k3_ribbon_drawers
+  end
+
+  def k3_ribbon_add_item
     @k3_ribbon_items ||= []
     @k3_ribbon_items << capture { yield }
+    nil
+  end
+
+  def k3_ribbon_add_drawer(name)
+    @k3_ribbon_drawers ||= Hash.new('')
+    @k3_ribbon_drawers[name] = render_cell('k3/ribbon', :drawer, :name => name, :content => capture { yield })
     nil
   end
 
