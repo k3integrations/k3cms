15e87fbcb1f30a6cc826f0b369e6131c57ef3c99
Tyler Rick <tyler@k3integrations.com>
2011-01-28 11:54:58 -0800

    Changes to match the new HTML structure of k3_ribbon
    Also, this CSS was causing an unwanted white stripe at the top of the
    page:
    .k3_section {
      margin-bottom: 5px;
    }

--- public/javascripts/k3/inline_editor.js
+++ public/javascripts/k3/inline_editor.js
@@ -258,12 +258,10 @@ function initInlineEditor(options) {
     },
   });
 
-  $ribbon = $('#k3_ribbon .row_2')
-  if ($ribbon.length == 0)
-    $ribbon = $('#k3_ribbon')
+  $pane = $('#k3_ribbon .panes .edit')
   $toolbar = $('<div class="k3_inline_editor k3_section k3_inline_editor_icons"></div>').
     append('<ul></ul>').
-    appendTo($ribbon);
+    appendTo($pane);
   $ul = $toolbar.find('ul');
   
   // Add all the buttons to the toolbar, according to the configuration stored in toolbar_options
@@ -362,7 +360,7 @@ function initInlineEditor(options) {
   var dwr_def = drawer_defaults;
   for (var dwr_id in drawer_options) {
     var dwr = drawer_options[dwr_id];
-    $('#k3_ribbon_beneath').before(drawerContents(dwr_id, '', dwr.fields));
+    $('#k3_drawers').before(drawerContents(dwr_id, '', dwr.fields));
     $('.' + dwr_id + '.drawer').bind('open', {dwr_id: dwr_id, dwr: dwr}, function(event) {
       var editable = event.data.dwr.get_editable();
       if (editable == null) {
