3b79b1994baa4ecc63277bfeb7faac8f65f13ea6
Tyler Rick <tyler@k3integrations.com>
2011-03-15 09:25:15 -0700

    Switched from Jeweler to Bundler

--- lib/k3_core.rb
+++ lib/k3_core.rb
@@ -1,4 +1,2 @@
-if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  require File.dirname(__FILE__) + '/k3/engine' 
-  require File.dirname(__FILE__) + '/k3/file_utils'
-end
+require File.dirname(__FILE__) + '/k3/engine'
+require File.dirname(__FILE__) + '/k3/file_utils'
