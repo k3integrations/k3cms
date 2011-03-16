c2f9de72cf8fe50f44b0665cdce040877bcbb6c3
Tyler Rick <tyler@k3integrations.com>
2011-03-16 16:13:59 -0700

    Explicitly require some gems since Rails/Bundler doesn't do it for us

--- lib/k3/ribbon/engine.rb
+++ lib/k3/ribbon/engine.rb
@@ -6,8 +6,8 @@ require 'facets/pathname'
 
 module K3
   module Ribbon
-
     class Engine < Rails::Engine
+
       config.action_view.javascript_expansions[:k3] ||= []
       config.action_view.javascript_expansions[:k3].concat [
         'jquery.tools.min.js',
@@ -17,6 +17,14 @@ module K3
 
       initializer 'k3.ribbon.add_cells_paths' do |app|
         Cell::Base.view_paths += [Pathname[__DIR__] + '../../../app/cells']
+      end
+
+      config.before_initialize do
+        # Work around the fact that the line:
+        #   Bundler.require(:default, Rails.env) if defined?(Bundler)
+        # in config/application.rb only does a 'require' for the gems explicitly listed in the app's Gemfile -- not for the gems *they* might depend on.
+        require 'haml'
+        #require 'haml-rails'
       end
 
       config.to_prepare do
@@ -31,7 +39,7 @@ module K3
           include K3::Ribbon::RibbonHelper
         end
       end
-    end
 
+    end
   end
 end
