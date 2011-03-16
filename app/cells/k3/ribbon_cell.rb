21d8880ea01d91db2e3b692dd826c1ca4d6f71d9
Tyler Rick <tyler@k3integrations.com>
2011-03-16 16:19:34 -0700

    Fix deprecation warnings from cells

--- app/cells/k3/ribbon_cell.rb
+++ app/cells/k3/ribbon_cell.rb
@@ -3,7 +3,7 @@ class K3::RibbonCell < Cell::Base
   helper K3::Ribbon::RibbonHelper
 
   def show
-    @page = @opts[:current_page]
+    @page = @options[:current_page]
     render if k3_user.k3_permitted?(:view_ribbon)
   end
 
