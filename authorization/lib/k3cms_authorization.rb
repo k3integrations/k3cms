b632fbd7ed2489ee3a02b07702a7fe563ab75ca3
Tyler Rick <tyler@k3integrations.com>
2011-04-26 17:23:59 -0700

    authorization: Made it automatically detect if Devise is available and load the Devise driver if it is.

--- authorization/lib/k3cms_authorization.rb
+++ authorization/lib/k3cms_authorization.rb
@@ -1,5 +1,5 @@
-require 'k3cms/authorization/railtie' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 require 'k3cms/authorization/engine_additions'
+require 'k3cms/authorization/engine'
 require 'k3cms/authorization/authorization_set'
 require 'k3cms/authorization/parser'
 require 'k3cms/authorization/permission_set'
