28a1e7e918512ec8a8e3f02c8d2ea37b0ffce775
Tyler Rick <tyler@k3integrations.com>
2011-05-02 14:23:33 -0700

    Moved some lines around

--- pages/app/controllers/k3cms/pages/base_controller.rb
+++ pages/app/controllers/k3cms/pages/base_controller.rb
@@ -1,10 +1,10 @@
-require 'cancan'
-
 module K3cms
   module Pages
     class BaseController < ApplicationController
-      # Needed for CanCan authorization
+
       include CanCan::ControllerAdditions
+      helper K3cms::InlineEditor::InlineEditorHelper
+      include K3cms::Pages::PagesHelper
 
       def current_ability
         @current_ability ||= K3cms::Pages::Ability.new(k3cms_user)
@@ -14,8 +14,6 @@ module K3cms
         k3cms_authorization_required(exception)
       end
 
-      helper K3cms::InlineEditor::InlineEditorHelper
-      include K3cms::Pages::PagesHelper
     end
   end
 end
