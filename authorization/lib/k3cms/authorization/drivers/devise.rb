549cb44d171bcbc1db138d318d6d2fdc393f39cc
Tyler Rick <tyler@k3integrations.com>
2011-05-06 00:15:45 -0700

    Cleaned up authorization/engine.rb by moving some code out to authorization/drivers/devise.rb.
    Discovered delegate! Now we don't have to do as much work in our cells,
    because we can delegate methods from the cell to the parent_controller.
    Starting with k3cms_user. This is better than having to include
    K3cms::Authorization::Drivers::Devise and other modules into both the
    Cells base class and the normal controller base class.

--- authorization/lib/k3cms/authorization/drivers/devise.rb
+++ authorization/lib/k3cms/authorization/drivers/devise.rb
@@ -4,12 +4,12 @@ module K3cms
       module Devise
         extend ActiveSupport::Concern
 
-        private
-
         # Devise specific
         def k3cms_user
           current_user || @k3cms_guest_user ||= K3cms::Authorization::GuestUser.new
         end
+
+        private
 
         def k3cms_authorization_required(exception = nil)
           if !user_signed_in?
@@ -40,4 +40,17 @@ module K3cms
       end
     end
   end
+end
+
+ActiveSupport.on_load(:action_controller) do
+  include K3cms::Authorization::Drivers::Devise
+  include K3cms::Authorization::GeneralControllerMethods
+
+  class Cell::Rails
+    # Too bad delegate :current_ability doesn't work...
+    delegate :k3cms_user, :to => :parent_controller
+  end
+  #Cell::Base.send :include, Devise::Controllers::Helpers
+  #Cell::Base.send :include, K3cms::Authorization::Drivers::Devise
+  #Cell::Base.send :include, K3cms::Authorization::GeneralControllerMethods
 end
--- authorization/lib/k3cms/authorization/engine.rb
+++ authorization/lib/k3cms/authorization/engine.rb
@@ -20,16 +20,8 @@ module K3cms
       # If *Devise* is loaded, load some necessary Devise modules.
       # TODO: can we move all of this into k3cms/authorization/drivers/devise.rb?
       initializer 'k3.authorization.devise' do
-        ActiveSupport.on_load(:action_controller) do
-          if defined?(::Devise)
+        if defined?(::Devise)
             require 'k3cms/authorization/drivers/devise'
-            include K3cms::Authorization::Drivers::Devise
-            include K3cms::Authorization::GeneralControllerMethods
-
-            Cell::Base.send :include, Devise::Controllers::Helpers
-            Cell::Base.send :include, K3cms::Authorization::Drivers::Devise
-            Cell::Base.send :include, K3cms::Authorization::GeneralControllerMethods
-          end
         end
       end
 
