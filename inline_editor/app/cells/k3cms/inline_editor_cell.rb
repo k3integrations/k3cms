16079669c24d071e13a4a8c5862f56b5b6ebfa8f
Tyler Rick <tyler@k3integrations.com>
2011-05-10 15:44:23 -0700

    Extracted the JavaScript code for showing/hiding the context ribbon for a record box out of s3_podcast gem and made it generic enough that it should be usable for pretty much any kind of record, hopefully.

--- inline_editor/app/cells/k3cms/inline_editor_cell.rb
+++ inline_editor/app/cells/k3cms/inline_editor_cell.rb
@@ -1,10 +1,16 @@
 class K3cms::InlineEditorCell < Cell::Base
-
-  helper  K3cms::Ribbon::RibbonHelper
-  include K3cms::Ribbon::RibbonHelper # for edit_mode?
   helper K3cms::InlineEditor::InlineEditorHelper
+  include ActionController::RecordIdentifier
 
   def init_edit_mode
+    render if edit_mode?
+  end
+
+  def record_editing_js(record, options = {})
+    @record          = record
+    @extra_params    = options[:extra_params] || {}
+    @new_record_path = options[:new_record_path] || send("new_#{dom_class(@record)}_path")
+    @ribbon_offset   = options[:ribbon_offset] || [-0, -5] # (y, x)
     render if edit_mode?
   end
 
