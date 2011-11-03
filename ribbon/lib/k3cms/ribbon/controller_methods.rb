8d24504d7ea7d610dbbf067dd240007d9af2bd8b
Tyler Rick <tyler@k3integrations.com>
2011-11-03 11:42:56 -0700

    Require edit_mode assets (javascript_include_tag :k3cms_editing) if can_enable_edit_mode?, not just if edit_mode?.
    This is looking forward to a brighter day when edit mode can be toggled
    entirely on the client-side and doesn't require reloading the page.

--- ribbon/lib/k3cms/ribbon/controller_methods.rb
+++ ribbon/lib/k3cms/ribbon/controller_methods.rb
@@ -16,22 +16,36 @@ module K3cms
         if params[:edit].present?
           cookies[:edit_mode] = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(params[:edit])
         end
-      end
 
-      def edit_mode?
-        # This wouldn't work if we ever wanted GuestUser's to be able to edit too (like a wiki), but this works for now...
-        if cookies[:edit_mode] && K3cms::Authorization::GuestUser === k3cms_user
-          Rails.logger.debug "... Logged in as guest. Resetting edit_mode to false."
+        if not can_enable_edit_mode?
+          Rails.logger.debug "... Insufficient permissions. Resetting edit_mode to false."
           cookies[:edit_mode] = false
         end
         cookies[:edit_mode] = false if cookies[:edit_mode].nil?
+
         # cookies apparently only stores/returns strings, so convert to boolean
         cookies[:edit_mode] = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(cookies[:edit_mode])
+      end
+
+      # Returns true if current user has permission to enable edit mode.
+      # Used to determine whether to load the JavaScript code required for edit mode.
+      # This is looking forward to a brighter day when edit mode can be toggled entirely on the client-side and doesn't require reloading the page.
+      def can_enable_edit_mode?
+        # This wouldn't work if we ever wanted GuestUser's to be able to edit too (like a wiki), but this works for now...
+        if K3cms::Authorization::GuestUser === k3cms_user
+          false
+        else
+          !!k3cms_user
+        end
+      end
+
+      def edit_mode?
+        set_edit_mode
         cookies[:edit_mode]
       end
 
       included do
-        helper_method :edit_mode?
+        helper_method :can_enable_edit_mode?, :edit_mode?
       end
     end
   end
